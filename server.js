ddec3d88ec025a167058f1fb71f142993c71af08
Devansh Mehra <81522384+Devansh-Mehra@users.noreply.github.com>
2023-09-08 16:08:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/bfhl/data', (req, res) => {
+// CORS middleware to allow cross-origin requests
+app.use(cors());
+
+// Rate limiting middleware for /bfhl/data endpoint
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use('/bfhl/data', limiter);
+
+// Custom error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    error: err.message || 'Something went wrong!',
+  });
+});
+
+app.post('/bfhl/data', (req, res, next) => {
   const requestData = req.body;
 
   if (!requestData.data || !Array.isArray(requestData.data)) {
-    return res.status(400).json({
-      error: 'Invalid request data. The "data" property must be an array.',
-    });
+    const error = new Error(
+      'Invalid request data. The "data" property must be an array.'
+    );
+    error.status = 400;
+    return next(error); // Pass the error to the error handling middleware
   }
 
   const numbers = [];
@@ -54,6 +77,13 @@ app.post('/bfhl/data', (req, res) => {
 app.get('/bfhl', (req, res) => {
   res.status(200).json({
     operation_code: 1,
+  });
+});
+
+// Handling undefined routes
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: 'Route not found',
   });
 });
 
